bc396c38e643def2dedeae562ebec13c72d9e7b2
penpicha4412 <73680314+penpicha4412@users.noreply.github.com>
2020-11-27 08:54:37 +0700

    Create From.php

---
+++ From.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+     <title>Comment From</title>
+</head>
+<body>
+  <from action = "insert.php method = "post" id="CommentFrom">
+    Name:<br>
+    <input type="text" name= "name" id="idName" placeholder="Enter Name"> <br>
+    Comment:<br>
+    <textarea rows="10" cols="20" name="comment" id="idComment" placeholder="Enter Comment“></textarea><br>
+     Link:<br>
+<input type=
+<input type="submit" id="commentBtn">
+</from>
+</body>
+</html>
